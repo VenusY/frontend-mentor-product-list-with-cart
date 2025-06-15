bd807f156a2e1b7cbb14ee349185c372cf26c099
Venus <venusyip@outlook.com>
2025-06-15 12:25:21 +0100

    Create ProductList and Cart components and use in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
-export default function App() {}
+import './styles/reset.css';
+import ProductList from './components/ProductList';
+import Cart from './components/Cart';
+
+export default function App() {
+  return (
+    <>
+      <ProductList />
+      <Cart />
+    </>
+  );
+}
