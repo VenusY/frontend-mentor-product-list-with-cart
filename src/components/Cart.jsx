1dd765c2659e619ec76e12d062002b895f54219c
Venus <venusyip@outlook.com>
2025-06-16 21:21:39 +0100

    Use Order component in Cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,12 +1,12 @@
 import CartPlaceholder from './CartPlaceholder';
-import CartItems from './CartItems';
+import Order from './Order';
 
 export default function Cart({ emptyCart }) {
   return (
     <section className='cart'>
       <h2 className='cart__heading'>Your Cart</h2>
 
-      {emptyCart ? <CartPlaceholder /> : <CartItems />}
+      {emptyCart ? <CartPlaceholder /> : <Order />}
     </section>
   );
 }
