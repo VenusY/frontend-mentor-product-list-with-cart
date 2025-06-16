dfc41ff89c6e237c650ddd843f86567537dec8f4
Venus <venusyip@outlook.com>
2025-06-16 20:05:46 +0100

    Add markup for OrderTotal component

---
+++ src/components/OrderTotal.jsx
@@ -0,0 +1,8 @@
+export default function OrderTotal() {
+  return (
+    <section className='order-total'>
+      <h3 className='order-total__heading'>Order Total</h3>
+      <p className='order-total__total'>{}</p>
+    </section>
+  );
+}
