c35e09fa3a7e62bf50c84753f6a886c80cca8bb8
Venus <venusyip@outlook.com>
2025-06-16 15:26:31 +0100

    Create emptyCart state variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './styles/reset.css';
+import { useState } from 'react';
 import ProductList from './components/ProductList';
 import Cart from './components/Cart';
 
 export default function App() {
+  const [emptyCart, setEmptyCart] = useState(true);
+
   return (
     <>
       <ProductList />
-      <Cart />
+      <Cart emptyCart={emptyCart} />
     </>
   );
 }
