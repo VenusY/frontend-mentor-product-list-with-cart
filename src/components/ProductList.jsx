845278468899fb160ca833f1ddd99b8128bce291
Venus <venusyip@outlook.com>
2025-06-16 14:04:29 +0100

    Add markup for ProductList component

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,1 +1,10 @@
-export default function ProductList() {}
+import Menu from './Menu/Menu';
+
+export default function ProductList() {
+  return (
+    <section className='product-list'>
+      <h1 className='product-list__heading'>Desserts</h1>
+      <Menu />
+    </section>
+  );
+}
