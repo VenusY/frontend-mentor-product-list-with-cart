ccb5afa46a51b84fdc0ab6b281925ae72024a50c
Venus <venusyip@outlook.com>
2025-06-16 15:44:27 +0100

    Add markup for Cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,1 +1,12 @@
-export default function Cart() {}
+import CartPlaceholder from './CartPlaceholder';
+import CartItems from './CartItems';
+
+export default function Cart({ emptyCart }) {
+  return (
+    <section className='cart'>
+      <h2 className='cart__heading'>Your Cart</h2>
+
+      {emptyCart ? <CartPlaceholder /> : <CartItems />}
+    </section>
+  );
+}
