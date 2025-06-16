f733931824fc462985ec395490a742c466f627ec
Venus <venusyip@outlook.com>
2025-06-16 14:35:01 +0100

    Add markup for CartPlaceholder component

---
+++ src/components/CartPlaceholder.jsx
@@ -0,0 +1,14 @@
+export default function CartPlaceholder() {
+  return (
+    <div className='cart-placeholder'>
+      <img
+        src={require('../assets/images/illustration-empty-cart.svg')}
+        alt='Cake'
+        className='cart-placeholder__image'
+      />
+      <p className='cart-placeholder__text'>
+        Your added items will appear here
+      </p>
+    </div>
+  );
+}
