118c9e8044927719bcf6737e2c805d839ce76be9
Venus <venusyip@outlook.com>
2025-06-17 20:24:38 +0100

    Create new context and change default value of addedItems to Map object

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import './styles/reset.css';
 import { useState } from 'react';
+import { PageContext } from './contexts/PageContext';
 import ProductList from './components/ProductList';
 import Cart from './components/Cart';
 
 export default function App() {
   const [emptyCart, setEmptyCart] = useState(true);
-  const [addedItems, setAddedItems] = useState([]);
+  const [addedItems, setAddedItems] = useState(new Map());
   const [displayModal, setDisplayModal] = useState(false);
 
   return (
-    <>
+    <PageContext.Provider
+      value={{
+        emptyCart,
+        setEmptyCart,
+        addedItems,
+        setAddedItems,
+        displayModal,
+        setDisplayModal,
+      }}
+    >
       <ProductList />
       <Cart emptyCart={emptyCart} />
-    </>
+    </PageContext.Provider>
   );
 }
