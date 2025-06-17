42bdefaca57d09e7bd58580e36d84d7f848d4a7a
Venus <venusyip@outlook.com>
2025-06-17 21:14:08 +0100

    Create OrderItem component and add markup

---
+++ src/components/OrderItem.jsx
@@ -0,0 +1,23 @@
+export default function OrderItem({ name, quantity, price }) {
+  return (
+    <div className='order-item'>
+      <div className='order-item__info'>
+        <p className='order-item__name'>{name}</p>
+
+        <div className='order-item__container'>
+          <p className='order-item__quantity'>{quantity}</p>
+          <p className='order-item__price'>{price}</p>
+          <p className='order-item__total'>{price * quantity}</p>
+        </div>
+      </div>
+
+      <button className='order-item__remove-button'>
+        <img
+          src={require('../assets/images/icon-remove-item.svg')}
+          alt='Remove'
+          className='order-item__remove-icon'
+        />
+      </button>
+    </div>
+  );
+}
