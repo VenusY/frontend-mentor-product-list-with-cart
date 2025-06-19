b5a755a6971d8ce22020b8ce3c954cf2fa90658a
Venus <venusyip@outlook.com>
2025-06-19 13:54:29 +0100

    Add markup for OrderList component

--- src/components/OrderList.jsx
+++ src/components/OrderList.jsx
@@ -1,1 +1,21 @@
-export default function OrderList() {}
+import { useContext } from 'react';
+import { PageContext } from '../contexts/PageContext';
+import OrderItem from './OrderItem';
+
+export default function OrderList() {
+  const { addedItems } = useContext(PageContext);
+  const addedItemsArray = [...addedItems.entries()];
+
+  return (
+    <div>
+      {addedItemsArray.map((item) => (
+        <OrderItem
+          key={item[0].name}
+          name={item[0].name}
+          quantity={item[1]}
+          price={item[0].price}
+        />
+      ))}
+    </div>
+  );
+}
