b9aa5a2a097c675c72fce2b76a8a9c8ec6311b4e
Venus <venusyip@outlook.com>
2025-06-20 19:22:18 +0100

    Add quantity to heading in cart section

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,16 @@
 import CartPlaceholder from './CartPlaceholder';
 import Order from './Order';
 
-export default function Cart({ emptyCart }) {
+export default function Cart({ emptyCart, addedItems }) {
+  let quantity = 0;
+
+  for (let [key, value] of addedItems) {
+    quantity += value;
+  }
+
   return (
     <section className='cart'>
-      <h2 className='cart__heading'>Your Cart</h2>
+      <h2 className='cart__heading'>Your Cart ({quantity})</h2>
 
       {emptyCart ? <CartPlaceholder /> : <Order />}
     </section>
