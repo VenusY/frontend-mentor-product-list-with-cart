f96061a653f505e4b7a22a9ac32d704bc0ccbe97
Venus <venusyip@outlook.com>
2025-06-21 00:46:25 +0100

    Wrap part of sustainability statement in span tags

--- src/components/OrderInfo.jsx
+++ src/components/OrderInfo.jsx
@@ -6,7 +6,12 @@ export default function OrderInfo() {
         alt='Tree'
         className='order-info__tree-icon'
       />
-      <p className='order-info__message'>This is a carbon-neutral delivery</p>
+
+      <p className='order-info__message'>
+        This is a{' '}
+        <span className='order-info__message--bold'>carbon-neutral</span>{' '}
+        delivery
+      </p>
     </div>
   );
 }
