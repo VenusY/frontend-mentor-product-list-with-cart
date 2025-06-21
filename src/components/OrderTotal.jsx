1fd4dfe9ff7de6909a578660b3fe034e534191e2
Venus <venusyip@outlook.com>
2025-06-21 02:36:34 +0100

    Display total cost of all cart items

--- src/components/OrderTotal.jsx
+++ src/components/OrderTotal.jsx
@@ -1,10 +1,20 @@
 import '../styles/scss/OrderTotal.scss';
+import { useContext } from 'react';
+import { PageContext } from '../contexts/PageContext';
 
 export default function OrderTotal() {
+  const { addedItems } = useContext(PageContext);
+  let total = 0;
+
+  for (let [key, value] of addedItems) {
+    const cost = key.price * value;
+    total += cost;
+  }
+
   return (
     <section className='order-total'>
       <h3 className='order-total__heading'>Order Total</h3>
-      <p className='order-total__total'>{}</p>
+      <p className='order-total__total'>${total.toFixed(2)}</p>
     </section>
   );
 }
