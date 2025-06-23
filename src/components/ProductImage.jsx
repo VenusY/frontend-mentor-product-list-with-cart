bfca31ff28ceca5def6ca3875b369ae8e7951b2e
Venus <venusyip@outlook.com>
2025-06-23 18:09:23 +0100

    Add markup for ProductImage component

--- src/components/ProductImage.jsx
+++ src/components/ProductImage.jsx
@@ -1,1 +1,20 @@
-export default function ProductImage() {}
+import AddButton from './AddButton';
+
+export default function ProductImage({ name, image }) {
+  return (
+    <div className='product__image-container'>
+      <img
+        src={image[1]}
+        alt={name}
+        className='product__image product__mobile-image'
+      />
+      <img
+        src={image[3]}
+        alt={name}
+        className='product__image product__desktop-image'
+      />
+
+      <AddButton />
+    </div>
+  );
+}
