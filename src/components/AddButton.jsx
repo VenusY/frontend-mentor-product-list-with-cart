dbd2aacef4d3b0882a0a40cf13048acd8ac5c6b8
Venus <venusyip@outlook.com>
2025-06-23 15:06:32 +0100

    Create AddButton component

---
+++ src/components/AddButton.jsx
@@ -0,0 +1,12 @@
+export default function AddButton() {
+  return (
+    <button className='add-button'>
+      <img
+        src={require('../assets/images/icon-add-to-cart.svg')}
+        alt='Cart'
+        className='add-button__icon'
+      />
+      <span className='add-button__text'>Add to Cart</span>
+    </button>
+  );
+}
