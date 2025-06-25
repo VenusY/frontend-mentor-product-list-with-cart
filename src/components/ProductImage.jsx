f0d5f6c64eae31ef0af75a21ab5bd6260b2812d6
Venus <venusyip@outlook.com>
2025-06-25 02:51:40 +0100

    Display AddButton or ChangeQuantityPanel depending on whether product has been added

--- src/components/ProductImage.jsx
+++ src/components/ProductImage.jsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
+import { PageContext } from '../contexts/PageContext';
 import AddButton from './AddButton';
+import ChangeQuantityPanel from './ChangeQuantityPanel';
 
 export default function ProductImage({ name, image }) {
+  const { addedItems } = useContext(PageContext);
+
   return (
     <div className='product__image-container'>
       <img
@@ -14,7 +19,11 @@ export default function ProductImage({ name, image }) {
         className='product__image product__desktop-image'
       />
 
-      <AddButton />
+      {addedItems.has(name) ? (
+        <ChangeQuantityPanel name={name} />
+      ) : (
+        <AddButton />
+      )}
     </div>
   );
 }
