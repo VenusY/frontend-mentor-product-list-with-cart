951ce23cbd9c26eedd6cbddbafc46a4ced71596b
Venus <venusyip@outlook.com>
2025-06-27 17:34:05 +0100

    Create and add markup for Modal component

---
+++ src/components/Modal.jsx
@@ -0,0 +1,77 @@
+import { useContext } from 'react';
+import { PageContext } from '../contexts/PageContext';
+import ModalOrderItem from './ModalOrderItem';
+import OrderTotal from './OrderTotal';
+
+export default function Modal() {
+  const { addedItems } = useContext(PageContext);
+  const addedItemsArray = [...addedItems];
+  const productImageMap = new Map([
+    [
+      'Waffle with Berries',
+      require('../assets/images/image-waffle-thumbnail.jpg'),
+    ],
+    [
+      'Vanilla Bean Crème Brûlée',
+      require('../assets/images/image-creme-brulee-thumbnail.jpg'),
+    ],
+    [
+      'Macaron Mix of Five',
+      require('../assets/images/image-macaron-thumbnail.jpg'),
+    ],
+    [
+      'Classic Tiramisu',
+      require('../assets/images/image-tiramisu-thumbnail.jpg'),
+    ],
+    [
+      'Pistachio Baklava',
+      require('../assets/images/image-baklava-thumbnail.jpg'),
+    ],
+    [
+      'Lemon Meringue Pie',
+      require('../assets/images/image-meringue-thumbnail.jpg'),
+    ],
+    ['Red Velvet Cake', require('../assets/images/image-cake-thumbnail.jpg')],
+    [
+      'Salted Caramel Brownie',
+      require('../assets/images/image-brownie-thumbnail.jpg'),
+    ],
+    [
+      'Vanilla Panna Cotta',
+      require('../assets/images/image-panna-cotta-thumbnail.jpg'),
+    ],
+  ]);
+
+  return (
+    <div className='modal__background'>
+      <section className='modal'>
+        <img
+          src={require('../assets/images/icon-order-confirmed.svg')}
+          alt='Checkmark'
+          className='modal__icon'
+        />
+
+        <h2 className='modal__heading'>Order Confirmed</h2>
+
+        <p className='modal__message'>We hope you enjoy your food!</p>
+
+        <div className='modal__order'>
+          <div className='modal__order-list'>
+            {addedItemsArray.map((item) => (
+              <ModalOrderItem
+                image={productImageMap.get(item[0])}
+                name={item[0]}
+                price={item[1].price}
+                quantity={item[1].quantity}
+              />
+            ))}
+          </div>
+
+          <OrderTotal />
+        </div>
+
+        <button className='modal__new-order-button'>Start New Order</button>
+      </section>
+    </div>
+  );
+}
